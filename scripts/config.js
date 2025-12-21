4e390565c332a03a1073770b23dc77dc41b52079
Dora-Honor <dorahonor@gmail.com>
2025-12-21 20:09:29 +0900

    Add Vercel

--- scripts/config.js
+++ scripts/config.js
@@ -1,13 +1,20 @@
 // scripts/config.js
 hexo.on('ready', () => {
   const isNetlify = process.env.NETLIFY_CNAME === 'true';
+  const isVercel = process.env.VERCEL_CNAME === 'true';
   const config = hexo.config;
 
   if (isNetlify) {
     // Netlify 环境配置
     config.url = 'https://dhmskk.netlify.app';
     config.root = '/';
-  } else {
+  }
+  if (isVercel) {
+    // Vercel 环境配置
+    config.url = 'https://dhmskk.vercel.app';
+    config.root = '/';
+  }
+  else {
     // GitHub Pages 环境配置 (或其他默认环境)
     // 请将 YOUR_USERNAME 替换为你的 GitHub 用户名
     config.url = 'https://dorahonor.me';
